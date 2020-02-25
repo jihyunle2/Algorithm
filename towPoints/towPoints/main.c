853a44a935fb6252197c9bdcab4cb97cd23a56a3
jihyunle2 <ljh3795@naver.com>
2020-02-26 01:01:44 +0900

    modify codes

--- towPoints/towPoints/main.c
+++ towPoints/towPoints/main.c
@@ -2,48 +2,40 @@
 #include <math.h>
 
 int main() {
-    int n,i,j;
+    int n,i;
     int x[10000],y[10000];
-    int distance[10000][10000];
-    int index1_x = 0, index1_y = 0, index2_x = 0, index2_y= 0, min;
+    int distance[10000];
+    int index1 = 0, index2= 0, min;
     scanf( "%d" ,&n);
     
     for (i=0; i<n; i++){
         scanf("%d %d",&x[i], &y[i]);
-        if(i >= 1){
-            for(j=0; j<i; i++){
-                distance[i-1][j] = pow(x[i]-x[j],2.0) + pow(y[i]-y[j],2.0);
-            }
-        }
+    }
+    
+    for (i=0; i<n; i++) {
+        distance[i] = pow(x[i]-x[0],2.0) + pow(y[i]-y[0],2.0);
+        printf("%d ",distance[i]);
     }
     
     min = 10000000;
-    for (i=0; i<n; i++){
-        for(j=0; j<i; j++){
-            if(min > distance[i][j]){
-                min = distance[i][j];
-                index1_x = i+1;
-                index1_y = j;
-            }
+    for (i=1; i<n; i++){
+        if(min > distance[i]){
+            min = distance[i];
+            index1 = i;
         }
     }
-    int tmp = distance[index1_x][index1_y];
-    distance[index1_x][index1_y] = distance[0][0];
-    distance[0][0] = tmp;
+    
+    distance[0] = distance[index1];
     
     min = 10000000;
-    for (i=0; i<n; i++){
-        for(j=1; j<i; j++){
-            if(min > distance[i][j]){
-                min = distance[i][j];
-                index2_x = i+1;
-                index2_y = j;
-            }
+    for (i=1; i<n; i++){
+        if(index1 != i && min > distance[i]){
+            min = distance[i];
+            index2 = i;
         }
     }
 
-    
-    printf("%d", (int)(pow(x[index1_x]-x[index2_x],2.0) + pow(y[index1_y]-y[index2_y],2.0)));
+    printf("\n %d %d %d", index1,index2,(int)(pow(x[index1]-x[index2],2.0) + pow(y[index1]-y[index2],2.0)));
     
     return 0;
 }
